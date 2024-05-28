1342635228667f22aa5a1b24259ad0864db18b16
yuichi.sasamoto <kp258584@icloud.com>
2024-05-28 10:25:49 +0900

    Delete unnecessary files

--- domains/person/PersonService.ts
+++
@@ -1,5 +0,0 @@
-import User from '@/domains/person/User'
-
-export default interface PersonService {
-    initUserInfo: (user: User) => User;
-}
--- domains/person/User.ts
+++
@@ -1,7 +0,0 @@
-export default class User {
-    constructor(
-        public id: string = '', // 型が明示的にわかる場合は型定義は書かなくてもOK
-        public firstName = '', // = ''をしたことで、自動で型推論をしてくれている
-        public lastName = ''
-    ) { }
-}
